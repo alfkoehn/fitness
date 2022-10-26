ae129c89b27fb2e36732820f9f45f1d7f7cb4bca
Alf Köhn <alf.koehn@posteo.net>
2022-10-26 12:31:34 +0200

    clean up code, add main function

--- dailymile.py
+++ dailymile.py
@@ -18,9 +18,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 #import numpy as np
 
-data_dir    = './'
-fname_in    = '11572144'
-
 
 def get_title( fname_in, silent=True ):
 #{{{
@@ -97,64 +94,15 @@ def get_waypoints( fname_in, silent=True, single_plot=False ):
 #}}}
 
 
-print( get_title(fname_in, silent=False) )
-print( get_waypoints(fname_in, single_plot=True, silent=False) )
-print('xxxxxxxx' )
+def main():
+    data_dir    = './'
+    fname_in    = '11572144'
+    print( get_title(fname_in, silent=False) )
+    print( get_waypoints(fname_in, single_plot=True, silent=False) )
 
-with open (fname_in, 'r') as f:
-    # open html file and read its conten
-    contents = f.read()
 
-    # create BeautifulSoup object, pass HTML data to the constructor, specify parser
-    soup    = BeautifulSoup(contents, 'lxml')
+if __name__ == '__main__':
+    main()
 
-    # print html code of tags h2 and head
-    #print(soup.h2)
-    #print(soup.head)
 
-    # of the multiple li elements, first is printed
-    #print(soup.li)
-
-print('*********************')
-with open (fname_in, 'r') as fh:
-    soup2   = BeautifulSoup(fh, 'lxml')
-print(soup2.h2)
-rawJ    = soup2.find_all('script')
-print('len(rawJ): ',len(rawJ))
-#print(rawJ[6])
-J = str(rawJ[6])
-
-# split() method splits string from specified separator and returns list object 
-# with string elements separated by separator
-J1 = J.split('var waypoints = ')
-J2 = J1[1].split(';')
-J3 = J2[0]
-# delete beginning '[' and ending ']'
-J3 = J3[1:-1]
-
-# loop through string of waypoints and convert it into array
-lat = []
-lon = []
-print('len(J3):', len(J3))
-for ii in range(len(J3)):
-    if J3[ii] == '[':
-        # read until next occurence of ',' and convert into float
-        lat_val = float( J3[(ii+1):].partition(',')[0] )
-        #print( ii, J3[ii], lat_val )
-        lat.append( lat_val ) 
-    elif (J3[ii] == ',') and (J3[ii-1] != ']'):
-        lon.append( float( J3[(ii+1):].partition(']')[0] ) )
-
-#print(lat)
-#print(lon)
-
-#J4 = J3.split(',')
-
-#print(J3)
-#print(J3[0])
-
-fig1    = plt.figure( figsize=(8,6) )
-ax1     = fig1.add_subplot( 1,1,1 )
-ax1.plot( lon, lat, marker='.', linestyle='none' )
-plt.show()
 
